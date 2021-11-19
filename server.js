0d87823f45504dcc1c90799d0843ff4652806671
ronald <ronaldgutierrezj@gmail.com>
2021-11-19 16:35:13 -0600

    Middleware imported

--- server.js
+++ server.js
@@ -9,3 +9,36 @@ const { PORT = 3000 } = process.env;
 const express = require("express");
 // create application object
 const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(URL_MONGODB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  // Connection Events
+  mongoose.connection
+    .on("open", () => console.log("Your are connected to mongoose"))
+    .on("close", () => console.log("Your are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+  
+  ///////////////////////////////
+  // LISTENER
+  ////////////////////////////////
+  app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
