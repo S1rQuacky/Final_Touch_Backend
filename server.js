e53f8fe08510585da5fabca0ea8ebfcffc1edc5a
ronald <ronaldgutierrezj@gmail.com>
2021-11-19 16:11:35 -0600

    dependencies installed

---
+++ server.js
@@ -0,0 +1,11 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3000
+const { PORT = 3000 } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
